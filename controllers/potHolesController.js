ca387e422ee2ff86366be938c065589a15265d92
David Wachlin <davidwachlin@gmail.com>
2019-09-13 13:39:42 -0400

    full crud

--- controllers/potHolesController.js
+++ controllers/potHolesController.js
@@ -13,7 +13,7 @@ const PotholesController = {
   show: async (req, res) => {
     try {
       const potholeId = req.params.id;
-      const potholes = await Devs.findById(potholeId);
+      const potholes = await Potholes.findById(potholeId);
       res.json(potholes);
     } catch (err) {
       console.log(err);
@@ -24,8 +24,8 @@ const PotholesController = {
   create: async (req, res) => {
     try {
       const newPothole = req.body;
-      const savedPotholes = await PotHoles.create(newPothole);
-      res.json(savedPotholes);
+      const savedPothole = await Potholes.create(newPothole);
+      res.json(savedPothole);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
@@ -36,7 +36,7 @@ const PotholesController = {
     try {
       const potholeId = req.params.id;
       const updatedPothole = req.body;
-      const savedPothole = await Devs.findByIdAndUpdate(
+      const savedPothole = await Potholes.findByIdAndUpdate(
         potholeId,
         updatedPothole,
         {
---
+++ models/testdata.js
@@ -0,0 +1,7 @@
+const pothole = 
+{
+    lat: '33.76459',
+    long: '-84.34307',
+    severity: 'bad',
+    isFixed: false
+}
