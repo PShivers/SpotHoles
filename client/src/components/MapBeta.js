d92a6dc2a8a716643c0c94117c58091ca902e7fc
Paul Shivers <paulsshivers@gmail.com>
2019-09-25 15:54:13 -0400

    got generic info window to display on click

--- client/src/components/MapBeta.js
+++ client/src/components/MapBeta.js
@@ -1,37 +1,51 @@
 import React, { Component } from 'react';
-import {Map, GoogleApiWrapper, Marker, } from 'google-maps-react';
+import {Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 
 class MapContainer extends Component {
-    state = { 
-      newMarker: {name: "newMarker", lat: Number, lng: Number}      
+  state = { 
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedMarker: {},
+    selectedPlace: {},
+    newMarker: {name: "newMarker", lat: Number, lng: Number}      
   }
 
-  showInfoWindow =()=>{
-    console.log('marker clicked')
+  onMarkerClick =(markerProps, marker, e) => {
+    console.log(markerProps)
+    this.setState({
+      selectedPlace: markerProps,
+      activeMarker: marker,
+      showingInfoWindow: true
+    })
+  };
+
+  mapClicked=(mapProps, map, clickEvent)=>{
+    console.log(this.state)
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }else{
+      const latitude = clickEvent.latLng.lat();
+      const longitude = clickEvent.latLng.lng();
+      const newMarker = {...this.state.newMarker};
+      newMarker.lat = latitude;
+      newMarker.lng = longitude;       
+      this.setState({newMarker});
+    }
   }
 
-    mapClicked=(mapProps, map, clickEvent)=>{
-        const latitude = clickEvent.latLng.lat();
-        const longitude = clickEvent.latLng.lng();
-        const newMarker = {...this.state.newMarker};
-        newMarker.lat = latitude;
-        newMarker.lng = longitude;       
-        this.setState({newMarker});
-        console.log(this.state.newMarker)
-        // ...
-      }
-
-    render() { 
-      console.log(this.props)
-        return ( <div className="ui placeholder segment">
-        <Map
-            onClick={this.mapClicked}   
-            google={this.props.google}
-            zoom={9.5}
-            initialCenter={{ lat: 33.7490, lng: -84.3880}}
-        >
+  render() { 
+      return ( <div className="ui placeholder segment">
+      <Map
+          onClick={this.mapClicked}
+          google={this.props.google}
+          zoom={9.5}
+          initialCenter={{ lat: 33.7490, lng: -84.3880}}
+      >
         <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
         <Marker position={{ lat: 33.7490, lng: -84.380}} />
@@ -43,11 +57,18 @@ class MapContainer extends Component {
         <Marker 
           position={{lat:this.state.newMarker.lat, lng:this.state.newMarker.lng }}
           draggable={true}
-          onClick={this.showInfoWindow}
+          onClick={this.onMarkerClick}
         />
-        </Map>
-      </div> );
-    }
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    </div> );
+  }
 }
  
 export default GoogleApiWrapper({
